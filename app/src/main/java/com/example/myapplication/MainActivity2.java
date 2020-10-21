49e70260e83dfde85c3505995e80b82aab26b9d8
dagaroth00 <siddheshmane00@gmail.com>
2020-10-21 17:05:43 +0530

    added dashboard

--- app/src/main/java/com/example/myapplication/MainActivity2.java
+++ app/src/main/java/com/example/myapplication/MainActivity2.java
@@ -6,19 +6,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 
+import java.util.Objects;
+
 public class MainActivity2 extends AppCompatActivity {
 
     private static final String TAG = "MainActivity2";
-
+    FirebaseFirestore db =  FirebaseFirestore.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,6 +37,27 @@ public class MainActivity2 extends AppCompatActivity {
             name.setText(signInAccount.getDisplayName());
             code.setText(signInAccount.getId());
             Log.d(TAG, signInAccount.getDisplayName() + " and ID => " + signInAccount.getId());
+
+            DocumentReference docRef = db.collection("user").document(signInAccount.getId());
+            docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                @Override
+                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                    if (task.isSuccessful()) {
+                        DocumentSnapshot document = task.getResult();
+                        assert document != null;
+                        if (document.exists()) {
+                            Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                            Toast.makeText(getApplicationContext() ,"(document.getData()).get()",Toast.LENGTH_LONG);
+                            //Toast.makeText(this, "this is it ",Toast.LENGTH_LONG);
+                        } else {
+                            Log.d(TAG, "No such document");
+                        }
+                    } else {
+                        Log.d(TAG, "get failed with ", task.getException());
+                    }
+                }
+            });
+
         }
 
 
